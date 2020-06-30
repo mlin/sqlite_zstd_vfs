2a4fec3d0292fa6b1d34eeee767f9cb5081cf179
Mike Lin <dna@mlin.net>
2020-06-29 14:24:43 -1000

    simplify ThreadPool locking

--- src/ThreadPool.h
+++ src/ThreadPool.h
@@ -8,7 +8,6 @@
 
 #pragma once
 
-#include <atomic>
 #include <climits>
 #include <condition_variable>
 #include <functional>
@@ -26,6 +25,8 @@ class ThreadPool {
         std::function<void *(void *) noexcept> par;
         std::function<void(void *) noexcept> ser;
     };
+    std::function<bool(const Job &, const Job &)> job_greater_ =
+        [](const Job &lhs, const Job &rhs) { return lhs.seqno > rhs.seqno; };
 
     // main thread state
     size_t max_threads_, max_jobs_;
@@ -33,21 +34,16 @@ class ThreadPool {
     unsigned long long seqno_next_ = 0; // jobs enqueued so far / next seqno to be enqueued
 
     // shared state
-    std::atomic<unsigned long long> seqno_done_; // highest seqno done (serial stage completed)
-    std::mutex state_mutex_;
+    std::mutex mutex_;
     std::condition_variable cv_enqueue_, cv_done_;
     std::queue<Job> par_queue_;
-    bool shutdown_ = false;
-
-    // serial-stage state: priority queue to order jobs by seqno
-    std::mutex ser_mutex_;
-    std::function<bool(const Job &, const Job &)> job_greater_ =
-        [](const Job &lhs, const Job &rhs) { return lhs.seqno > rhs.seqno; };
     std::priority_queue<Job, std::vector<Job>, decltype(job_greater_)> ser_queue_;
+    unsigned long long seqno_done_ = 0; // highest seqno done (serial stage completed)
+    bool shutdown_ = false;
 
     // worker thread
     void Worker() {
-        std::unique_lock<std::mutex> lock(state_mutex_);
+        std::unique_lock<std::mutex> lock(mutex_);
         while (true) {
             // wait for / dequeue a job
             while (!shutdown_ && par_queue_.empty()) {
@@ -58,46 +54,38 @@ class ThreadPool {
             }
             Job job = par_queue_.front();
             par_queue_.pop();
-            lock.unlock();
             // run parallel stage
             if (job.par) {
+                lock.unlock();
                 job.x = job.par(job.x);
+                lock.lock();
             }
-            {
-                // enqueue serial stage
-                std::unique_lock<std::mutex> ser_lock(ser_mutex_);
-                ser_queue_.push(job);
-                // run the enqueued serial stage(s) so long as the next one is in seqno order.
-                // this may or may not include the one just enqueued.
-                while (!ser_queue_.empty()) {
-                    job = ser_queue_.top();
-                    if (job.seqno != seqno_done_) {
-                        break;
-                    }
-                    ser_queue_.pop();
-                    if (job.ser) {
-                        // run the next serial stage; we can release ser_lock for this because the
-                        // seqno check ensures only one can be dequeued at a time.
-                        ser_lock.unlock();
-                        job.ser(job.x);
-                        ser_lock.lock();
-                    }
-                    ++seqno_done_;
+            // enqueue serial stage
+            ser_queue_.push(job);
+            // run the enqueued serial stage(s) so long as the next one is in seqno order.
+            // this may or may not include the one just enqueued.
+            while (!ser_queue_.empty() && ser_queue_.top().seqno == seqno_done_) {
+                job = ser_queue_.top();
+                ser_queue_.pop();
+                if (job.ser) {
+                    // run the next serial stage; we can release lock for this because the seqno
+                    // check ensures only one can be dequeued at a time.
+                    lock.unlock();
+                    job.ser(job.x);
+                    lock.lock();
                 }
+                ++seqno_done_;
+                cv_done_.notify_all();
             }
-            lock.lock();
-            cv_done_.notify_all();
         }
     }
 
   public:
     ThreadPool(size_t max_threads, size_t max_jobs)
-        : max_threads_(max_threads), max_jobs_(max_jobs), ser_queue_(job_greater_) {
-        seqno_done_ = 0;
-    }
+        : max_threads_(max_threads), max_jobs_(max_jobs), ser_queue_(job_greater_) {}
     ~ThreadPool() {
         {
-            std::lock_guard<std::mutex> lock(state_mutex_);
+            std::lock_guard<std::mutex> lock(mutex_);
             shutdown_ = true;
             cv_enqueue_.notify_all();
         }
@@ -109,26 +97,27 @@ class ThreadPool {
     // Enqueue ser(par(x)) for background processing as described. The functions must not throw.
     void Enqueue(void *x, std::function<void *(void *) noexcept> par,
                  std::function<void(void *) noexcept> ser) {
-        std::unique_lock<std::mutex> lock(state_mutex_);
-        while (seqno_next_ - seqno_done_ >= max_jobs_) {
-            cv_done_.wait(lock);
-        }
         Job job;
-        job.seqno = seqno_next_++;
         job.x = x;
         job.par = par;
         job.ser = ser;
+
+        std::unique_lock<std::mutex> lock(mutex_);
+        while (seqno_next_ - seqno_done_ >= max_jobs_) {
+            cv_done_.wait(lock);
+        }
+        job.seqno = seqno_next_++;
         par_queue_.push(job);
+        cv_enqueue_.notify_one();
         if (threads_.size() < max_threads_ && threads_.size() < par_queue_.size()) {
             threads_.emplace_back([this]() { this->Worker(); });
         }
-        cv_enqueue_.notify_one();
     }
 
     // Await completion of all previously enqueued jobs
     void Barrier() {
         if (seqno_next_) {
-            std::unique_lock<std::mutex> lock(state_mutex_);
+            std::unique_lock<std::mutex> lock(mutex_);
             while (seqno_done_ < seqno_next_) {
                 cv_done_.wait(lock);
             }
